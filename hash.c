73e895ac9caf38ef7888417f0b497772d128019d
Franco Alonso <francoXavierAlonso@gmail.com>
2018-10-20 13:38:37 -0300

    hash_guardar, hash_obtener, hash_pertenece
    -Modificado hash_crear().
    -Agregado las funciones del titulo pero no estan definitivamete testeadas.

--- hash.c
+++ hash.c
@@ -13,7 +13,7 @@ typedef enum estado {
 } estado_t;
 
 typedef struct hash_campo {
-    char* clave;
+    const char* clave;
     void* valor;
     estado_t estado; 
 } hash_campo_t;
@@ -22,7 +22,7 @@ typedef struct hash {
 	size_t cantidad;                  
     size_t largo;                     
     size_t carga;                     
-    hash_campo_t* tabla; 
+    hash_campo_t** tabla; 
     hash_destruir_dato_t destruir_dato; 
 } hash_t;
 
@@ -31,17 +31,54 @@ typedef struct hash_iter {	//no estoy seguro si estos son los atributos del iter
 	size_t posicion;
 } hash_iter_t;
 
+/* Destruye la estructura liberando la memoria pedida y llamando a la función
+ * destruir para cada par (clave, dato).
+ * Pre: La estructura hash fue inicializada
+ * Post: La estructura hash fue destruida*/
+void hash_destruir(hash_t* hash) {
+	free(hash->tabla);
+	free(hash);
+}
+
+size_t funcion_hash(const char* clave, size_t largo_tabla) {
+    size_t hashval;
+    for (hashval = 0; *clave != '\0'; clave++) hashval = *clave + 31*hashval;
+    return hashval % largo_tabla;
+}
+
+hash_campo_t* campo_crear() {
+	hash_campo_t* campo = malloc(sizeof(hash_campo_t));
+	if(campo == NULL) return NULL;
+	campo->clave = NULL;
+	campo->valor = NULL;
+	campo->estado = libre;
+	return campo;
+}
+
+bool rellenar_tabla(hash_t* hash, int inicio) {
+	for(int i=inicio; i<hash->largo; i++) {
+		hash_campo_t* campo = campo_crear();
+		if(campo == NULL) {
+			hash_destruir(hash);
+			return false;
+		}
+		hash->tabla[i] = campo;
+	}
+	return true;
+}
+
 /* Crea el hash*/
 //preguntar por la carga del hash abierto.
 hash_t* hash_crear(hash_destruir_dato_t destruir_dato) {
 	hash_t* hash = malloc(sizeof(hash_t));
 	if(hash == NULL) return NULL;
-	hash_campo_t* tabla = malloc(sizeof(hash_campo_t) * LARGO_INICIAL);
+	hash_campo_t** tabla = malloc(sizeof(hash_campo_t*) * LARGO_INICIAL);
 	if(tabla == NULL) return NULL;
 	hash->tabla = tabla;
 	hash->cantidad = 0;
 	hash->largo = LARGO_INICIAL;
-	hash->carga = 0; //TODO, encontrar el funcionamiento del factor carga
+	hash->carga = 0;
+	if(!rellenar_tabla(hash, 0)) return NULL;
 	hash->destruir_dato = destruir_dato;
 	return hash;
 }
@@ -51,7 +88,17 @@ hash_t* hash_crear(hash_destruir_dato_t destruir_dato) {
  * Pre: La estructura hash fue inicializada
  * Post: Se almacenó el par (clave, dato)*/
 bool hash_guardar(hash_t* hash, const char* clave, void* dato) {
-	return false;
+	size_t posicion = funcion_hash(clave, hash->largo);
+	hash_campo_t** tabla = hash->tabla;
+	while(tabla[posicion]->estado != libre && posicion < hash->largo) {
+		posicion++;
+	}
+	hash_campo_t* campo = tabla[posicion];
+	campo->clave = clave;
+	campo->valor = dato;
+	campo->estado = ocupado;
+	hash->cantidad++;
+	return true;
 }
 
 /* Borra un elemento del hash y devuelve el dato asociado.  Devuelve
@@ -64,33 +111,36 @@ void* hash_borrar(hash_t* hash, const char* clave) {
 	return NULL;
 }
 
+void hasheando_por_hay(size_t* posicion, const hash_t* hash, const char* clave) {
+	while(hash->tabla[*posicion]->estado != libre && hash->tabla[*posicion]->clave != clave && *posicion < hash->largo) {
+		*posicion += 1;
+	}
+}
+
 /* Obtiene el valor de un elemento del hash, si la clave no se encuentra
  * devuelve NULL.
  * Pre: La estructura hash fue inicializada*/
 void* hash_obtener(const hash_t* hash, const char* clave) {
-	return NULL;
+	size_t posicion = funcion_hash(clave, hash->largo);
+	hasheando_por_hay(&posicion, hash, clave);
+	if(hash->tabla[posicion]->clave != clave) return NULL;
+	return hash->tabla[posicion]->valor;
 }
 
 /* Determina si clave pertenece o no al hash.
  * Pre: La estructura hash fue inicializada*/
 bool hash_pertenece(const hash_t* hash, const char* clave) {
-	return false;
+	size_t posicion = funcion_hash(clave, hash->largo);
+	hasheando_por_hay(&posicion, hash, clave);
+	return hash->tabla[posicion]->clave == clave;
 }
 
 /* Devuelve la cantidad de elementos del hash.
  * Pre: La estructura hash fue inicializada*/
 size_t hash_cantidad(const hash_t* hash) {
-	return 0;
+	return hash->cantidad;
 }
 
-/* Destruye la estructura liberando la memoria pedida y llamando a la función
- * destruir para cada par (clave, dato).
- * Pre: La estructura hash fue inicializada
- * Post: La estructura hash fue destruida*/
-void hash_destruir(hash_t* hash) {
-	free(hash->tabla);
-	free(hash);
-}
 
 /* Iterador del hash */
 
